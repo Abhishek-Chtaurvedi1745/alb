bf1dd1872d4541fd86837eeb35c35424a9d72a85
Aryan <107269876+pikachu71213@users.noreply.github.com>
2026-06-24 18:44:06 +0530

    add faq

--- app/project-management/page.jsx
+++ app/project-management/page.jsx
@@ -1,4 +1,45 @@
+"use client";
 import React from 'react'
+import { Plus, X } from "lucide-react";
+import { useState } from "react";
+
+const faqData = [
+  {
+    question:
+      "What is Project Portfolio Management (PPM) and how does Albatroz help?",
+    answer:
+      "Project Portfolio Management (PPM) helps organizations manage multiple projects efficiently by aligning them with business goals. Albatroz provides structured PPM solutions to improve visibility, prioritize the right projects, and optimize resource allocation.",
+  },
+
+  {
+    question:
+      "What kind of automation solutions does Albatroz offer?",
+    answer:
+      "Albatroz provides enterprise automation solutions including workflow automation, IT process automation, cloud automation, and operational efficiency tools tailored for businesses.",
+  },
+
+  {
+    question:
+      "What is Project Management as a Service (PMaaS)?",
+    answer:
+      "PMaaS allows organizations to outsource project management expertise and processes, helping businesses improve delivery timelines and project success rates.",
+  },
+
+  {
+    question:
+      "How does Business Intelligence (BI) benefit my organization?",
+    answer:
+      "Business Intelligence helps organizations make data-driven decisions using dashboards, analytics, reporting tools, and predictive insights.",
+  },
+
+  {
+    question:
+      "Can Albatroz customize these solutions for my business?",
+    answer:
+      "Yes, Albatroz provides customized enterprise solutions tailored according to business size, goals, industry requirements, and operational workflows.",
+  },
+];
+
 const benefits = [
   {
     icon: "/images/1.svg",
@@ -84,6 +125,11 @@ const features2 = [
 ];
 
 function page() {
+  const [active, setActive] = useState(0);
+
+  const toggleFAQ = (index) => {
+    setActive(active === index ? null : index);
+  };
   return (
     <div>
       <section className="bg-[#000000] py-16 px-6 mt-[83px]">
@@ -316,8 +362,8 @@ Get my AI readiness report
         </div>
       </div>
     </section>
-    <section className="bg-black pt-15 overflow-hidden max-w-[1600px]">
-  <div className=" mx-auto">
+    <section className="bg-black pt-15 overflow-hidden ">
+  <div className=" mx-auto max-w-[1600px]">
 
     <div className="grid lg:grid-cols-[55%_45%] items-start gap-10">
 
@@ -392,8 +438,8 @@ Get my AI readiness report
 
   </div>
 </section>
- <section className="bg-black py-15 overflow-hidden pt-20 max-w-[1600px]">
-  <div className=" mx-auto">
+ <section className="bg-black py-15 overflow-hidden pt-20 ">
+  <div className=" mx-auto max-w-[1600px]">
 
     <div className="grid lg:grid-cols-[55%_45%] items-start gap-10">
 
@@ -472,24 +518,112 @@ across an organization to deliver value to customers efficiently. It encompasses
       <div className="max-w-4xl mx-auto">
         
         {/* Top Section: Button aligned right */}
-       <div className="flex mb-0" style={{ justifyContent: 'end' }}>
+       <div className="flex mb-0" style={{ justifyContent: 'center' }}>
           <button className="bg-[#FF3B30] hover:bg-[#E03126] text-white font-semibold py-3 px-8 text-lg rounded-sm transition-colors duration-200 shadow-lg cursor-pointer">
             Book free Consultation
           </button>
         </div>
 
         {/* Line divider directly beneath the button */}
-        <hr className="border-t border-gray-800 mb-10 w-full" />
+        {/* <hr className="border-t border-gray-800 mb-10 w-full" /> */}
 
         {/* Content Paragraph */}
-        <p className="text-center text-[#FFFFFF] text-base md:text-lg leading-relaxed max-w-3xl mx-auto font-light tracking-wide">
+        {/* <p className="text-center text-[#FFFFFF] text-base md:text-lg leading-relaxed max-w-3xl mx-auto font-light tracking-wide">
           These management areas often intersect and complement each other, especially 
           in organizations aiming for digital transformation or seeking to enhance their 
           agility and competitiveness in today's rapidly evolving business landscape. 
           Integrating these practices can lead to more streamlined operations, improved 
           decision-making, and ultimately, better outcomes for the organization.
         </p>
-        
+         */}
+      </div>
+    </section>
+    <section className="w-full bg-black  py-3 md:py-13 pb-13 px-4 overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 items-start">
+
+          {/* LEFT SIDE */}
+          <div className="flex flex-col items-center lg:items-center">
+
+           <h2 className="text-white text-3xl md:text-[40px] font-semibold leading-tight mt-0 md:mt-0 mb-7">
+  Frequently 
+  <br className="hidden md:block" /> 
+  
+    Asked
+  <br className="hidden md:block" />
+  <span className="text-[#ff403a]"> Questions </span>
+</h2>
+            {/* IMAGE */}
+            <div className="mt-10 hidden md:block">
+              <img
+                src="images/faqs.png"
+                alt=""
+                className="w-full object-contain h-[400px]"
+              />
+            </div>
+
+          </div>
+
+          {/* RIGHT SIDE */}
+          <div className="space-y-5">
+
+            {faqData.map((item, index) => (
+              <div
+                key={index}
+                className={`border rounded-2xl overflow-hidden transition-all duration-500
+                ${
+                  active === index
+                    ? "border-[#ff403a] bg-[#111111]"
+                    : "border-gray-700 bg-black"
+                }`}
+              >
+
+                {/* QUESTION */}
+                <button
+                  onClick={() => toggleFAQ(index)}
+                  className="w-full flex items-center justify-between text-left p-6"
+                >
+
+                  <h3 className="text-white text-[16px] md:text-xl font-medium pr-5">
+                    {item.question}
+                  </h3>
+
+                  <span className="text-white flex-shrink-0">
+                    {active === index ? (
+                      <X size={24} className="text-[#ff403a]" />
+                    ) : (
+                      <Plus size={24} />
+                    )}
+                  </span>
+
+                </button>
+
+                {/* ANSWER */}
+                <div
+                  className={`grid transition-all duration-500 ease-in-out
+                  ${
+                    active === index
+                      ? "grid-rows-[1fr] opacity-100"
+                      : "grid-rows-[0fr] opacity-0"
+                  }`}
+                >
+                  <div className="overflow-hidden">
+
+                    <p className="text-[#FFFFFF] text-[12px] md:text-[14px] leading-relaxed px-6 pb-6 text-sm md:text-base">
+                      {item.answer}
+                    </p>
+
+                  </div>
+                </div>
+
+              </div>
+            ))}
+
+          </div>
+
+        </div>
+
       </div>
     </section>
     </div>
